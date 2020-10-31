78561e4e610e7c8b07a2366f17f050b4628226c6
venkatanumala <venkey.anusri@gmail.com>
2020-10-31 11:02:06 -0500

    added HelloApiView and sample Jsonresponse

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+""" Test APi View"""
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+        """Retrun a list of APIView features """
+        an_apiview=[
+            'Uses  HTTP methods as function (get,post,patch,put,delete)',
+            'Is similar to Traditional Django view',
+            'Gives you most control over your application',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
