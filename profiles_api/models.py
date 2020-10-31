d566d0c1c2ac2dbcccd8df843c820d5362c7ad07
venkatanumala <venkey.anusri@gmail.com>
2020-10-31 06:10:51 -0500

    create user time errors rectified on models.py file

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,29 +5,28 @@ from django.contrib.auth.models import BaseUserManager
 
 
 # Create your models here.
-
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self,email,name,password=None):
-        """create new user profile"""
-
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError("user must have an email address")
+            raise ValueError('Users must have an email address')
 
-        email=self.normalize_email(email)
-        user=self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-    def create_superuser(self,email,name,password):
-        """  Create and save a new super user with given details """
-        user=self.create_user(email,name,password)
-
-        user.is_superuser=True
-        user.is_staff=True
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
@@ -35,27 +34,25 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for the user in the system"""
-    email=models.EmailField(max_length=50, unique=True)
-    name = models.CharField(max_length=50)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    objects= UserProfileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        '''Retrive full name of user'''
-
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        '''Retrive short name of user'''
-
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        ''' retrun string representation of our user '''
+        """Return string representation of user"""
         return self.email
