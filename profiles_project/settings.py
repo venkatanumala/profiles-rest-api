cae1829be4e2a0fe1092edc82196460653f99975
venkatanumala <venkey.anusri@gmail.com>
2020-10-30 22:33:37 -0500

    added custom user profile model, manage and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'profiles_api',
 
 ]
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL='profiles_api.UserProfile'
